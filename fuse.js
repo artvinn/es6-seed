f4850a10d5469fa65b2b40cd919d8b77a90e77b1
Artjom <artjomvinnikov@gmail.com>
2017-10-01 15:07:21 +0300

    Use fusebox for bundling & dev-server

---
+++ fuse.js
@@ -0,0 +1,39 @@
+const { FuseBox, Sparky, BabelPlugin } = require("fuse-box");
+
+const fuse = FuseBox.init({
+  homeDir: "src",
+  output: "dist/$name.js",
+  sourceMaps: true,
+  cache: true,
+
+  plugins: [
+    BabelPlugin({
+      config: {
+        sourceMaps: true,
+        presets: ["env"]
+      }
+    })
+  ]
+});
+
+Sparky.task("dev-server", () => {
+  fuse.dev({ port: 8080 });
+
+  fuse.bundle("app")
+    .instructions(" > ./js/index.js")
+    .target("browser")
+    .watch()
+    .hmr({ reload: true });
+})
+
+Sparky.task("clean", () => {
+  return Sparky.src("dist/").clean("dist/");
+});
+
+Sparky.task("watch:static", () => {
+  return Sparky
+        .watch(["./assets", "index.html"], { base: "./src" })
+        .dest("./dist");
+})
+
+Sparky.task("default", ["clean", "watch:static", "dev-server"], () => fuse.run());
---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+window.onload = init;
+
+const hello = 4;
+
+function init() {
+  setInterval(() => {
+    console.log(hello)
+  }, 1000)
+}
